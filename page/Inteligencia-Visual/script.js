19392990e96b41a241a416f78c28e2d2709241d8
Isai Abreu <158493209+iabreuIjam99@users.noreply.github.com>
2026-08-21 15:11:57 -0400

    feat: download button in gallery lightbox

--- page/Inteligencia-Visual/script.js
+++ page/Inteligencia-Visual/script.js
@@ -4,6 +4,7 @@
   var gallery = document.getElementById("gallery");
   var lightbox = document.getElementById("lightbox");
   var lightboxImg = document.getElementById("lightboxImg");
+  var lightboxDownload = document.getElementById("lightboxDownload");
 
   function render(sections) {
     if (!sections || sections.length === 0) {
@@ -12,7 +13,7 @@
     }
     var html = sections.map(function (s) {
       var photos = s.fotos.map(function (f) {
-        return '<div class="gallery-item" data-url="' + f.url + '" data-name="' + (f.name || '') + '">' +
+        return '<div class="gallery-item" data-url="' + f.url + '" data-id="' + f.id + '" data-name="' + (f.name || '') + '">' +
                '<img loading="lazy" src="' + f.url + '" alt="' + (f.name || 'Foto') + '"/>' +
                '</div>';
       }).join("");
@@ -22,8 +23,9 @@
     gallery.innerHTML = html;
   }
 
-  function openLightbox(url) {
+  function openLightbox(url, id) {
     lightboxImg.src = url;
+    lightboxDownload.href = "https://drive.google.com/uc?export=download&id=" + id;
     lightbox.classList.remove("hidden");
     document.body.style.overflow = "hidden";
   }
@@ -31,12 +33,13 @@
   function closeLightbox() {
     lightbox.classList.add("hidden");
     lightboxImg.src = "";
+    lightboxDownload.href = "#";
     document.body.style.overflow = "";
   }
 
   gallery.addEventListener("click", function (e) {
     var item = e.target.closest(".gallery-item");
-    if (item) openLightbox(item.dataset.url);
+    if (item) openLightbox(item.dataset.url, item.dataset.id);
   });
   lightbox.addEventListener("click", closeLightbox);
   document.getElementById("lightboxClose").addEventListener("click", closeLightbox);
